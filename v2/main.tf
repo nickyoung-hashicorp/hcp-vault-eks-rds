0e2d46956d218796a81e66bae5753a59a20c2826
nickyoung-hashicorp <nyoung@hashicorp.com>
2022-05-13 15:22:41 -0700

    changes

--- v2/main.tf
+++ v2/main.tf
@@ -225,3 +225,10 @@ resource "aws_key_pair" "vault" {
   key_name   = local.private_key_filename
   public_key = tls_private_key.this.public_key_openssh
 }
+
+# Generate local PEM file
+resource "local_file" "private_key" {
+  content         = tls_private_key.this.private_key_pem
+  filename        = "ssh-key.pem"
+  file_permission = "0600"
+}
